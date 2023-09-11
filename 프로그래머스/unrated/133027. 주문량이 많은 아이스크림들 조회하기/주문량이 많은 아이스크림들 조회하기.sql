da0182f51c7703be2b7614ef6013dd33a980b2b0
PARKJUYOUNG <juzero98@naver.com>
2023-09-11 23:36:37 +0900

    [unrated] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,18 +1,30 @@
+-- SELECT FLAVOR
+-- FROM  (
+--         SELECT A.FLAVOR
+--             , ROW_NUMBER() OVER(ORDER BY A.TOT + B.TOT DESC) AS RN
+--         FROM (
+--                 SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOT
+--                 FROM FIRST_HALF
+--                 GROUP BY FLAVOR
+--              ) A
+--             , (
+--                 SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOT
+--                 FROM JULY
+--                 GROUP BY FLAVOR
+--              ) B
+--         WHERE A.FLAVOR = B.FLAVOR
+--         )
+-- WHERE RN < 4
+-- ;
+
+-- UNION 풀이
 SELECT FLAVOR
-FROM  (
-        SELECT A.FLAVOR
-            , ROW_NUMBER() OVER(ORDER BY A.TOT + B.TOT DESC) AS RN
-        FROM (
-                SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOT
-                FROM FIRST_HALF
-                GROUP BY FLAVOR
-             ) A
-            , (
-                SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOT
-                FROM JULY
-                GROUP BY FLAVOR
-             ) B
-        WHERE A.FLAVOR = B.FLAVOR
-        )
+FROM (
+    SELECT FLAVOR, ROW_NUMBER() OVER(ORDER BY SUM(TOTAL_ORDER) DESC) AS RN
+    FROM (SELECT * FROM FIRST_HALF
+        UNION
+        SELECT * FROM JULY)
+    GROUP BY FLAVOR
+    )
 WHERE RN < 4
 ;
